bd5749e8d2a3d2c1b865e1ae623f85f2c3efb6e3
Galina Korosteliov <gkorosteliov@ocls.ca>
2019-10-17 11:37:03 -0400

    moved defenition of custom_no_search_results_msg variable to template.php

--- templates/islandora-solr.tpl.php
+++ templates/islandora-solr.tpl.php
@@ -10,13 +10,8 @@
  */
 
 ?>
-<?php 
-$custom_no_search_results_msg=theme_get_setting('ocls_custom_no_search_results_msg');
-if (empty($custom_no_search_results_msg))
-{$custom_no_search_results_msg='Sorry, but your search returned no results.';}
-?>
 <?php if (empty($results)): ?>
-  <p class="no-results"><?php print t($custom_no_search_results_msg); ?></p>
+  <p class="no-results"><?php print $custom_no_search_results_msg; ?></p>
 <?php else: ?>
   <div class="islandora islandora-solr-search-results">
     <?php $row_result = 0; ?>
